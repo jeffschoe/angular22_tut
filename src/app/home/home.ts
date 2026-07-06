19da7218bc1b2062326d646d0ea889d893455b2c
jeffschoe <ratacame@protonmail.com>
2026-07-05 22:13:16 -0400

    04-interfaces

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
+import { HousinglocationInfo } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -17,6 +18,20 @@ import { HousingLocation } from '../housing-location/housing-location';
   `,
   styleUrls: [`./home.css`],
 })
+
 export class Home {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousinglocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
